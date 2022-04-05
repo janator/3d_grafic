00feca7e0bcd446f3e30df4836aaed99128838d4
janator <48880517+janator@users.noreply.github.com>
2022-04-05 21:07:23 +0300

    two triangles

---
+++ hw_two_triangles/two_triangles.cpp
@@ -0,0 +1,100 @@
+// Include standard headers
+#include <stdio.h>
+#include <stdlib.h>
+
+// Include GLEW
+#include <GL/glew.h>
+
+// Include GLFW
+#include <GLFW/glfw3.h>
+GLFWwindow* window;
+
+// Include GLM
+#include <glm/glm.hpp>
+using namespace glm;
+
+#include <common/shader.hpp>
+
+int main(void) {
+  if( !glfwInit() )
+  {
+    fprintf( stderr, "Failed to initialize GLFW\n" );
+    getchar();
+    return -1;
+  }
+  glfwWindowHint(GLFW_SAMPLES, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  window = glfwCreateWindow( 1024, 768, "Tutorial 02 - Red triangle", NULL, NULL);
+  if( window == NULL ){
+    fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
+    getchar();
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+  glewExperimental = true; // Needed for core profile
+  if (glewInit() != GLEW_OK) {
+    fprintf(stderr, "Failed to initialize GLEW\n");
+    getchar();
+    glfwTerminate();
+    return -1;
+  }
+  glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+  glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+  GLuint VertexArrayID;
+  glGenVertexArrays(1, &VertexArrayID);
+  glBindVertexArray(VertexArrayID);
+  GLuint programID = LoadShaders( "VertexShader.vertexshader", "ColorFragmentShader.fragmentshader" );
+  static const GLfloat g_vertex_buffer_data[] = {
+      -0.4f, -0.8f, 0.0f,
+      -0.4f, 0.8f, 0.0f,
+      0.4f, 0.0f, 0.0f,
+      0.0f, -0.8f, 0.0f,
+      0.0f, 0.8f, 0.0f,
+      0.8f, 0.0f, 0.0f
+  };
+  static const GLfloat g_color_buffer_data[] = {
+      0.8f, 0.0f, 0.8f, 1,
+      0.8f, 0.0f, 0.8f, 1,
+      1.0f, 0.0f, 0.0f, 1,
+      1.0f, 1.0f, 0.0f, 0.5,
+      1.0f, 1.0f, 0.0f, 0.5,
+      0.0f, 1.0f, 0.0f, 0.5
+  };
+  GLuint vertexbuffer;
+  glGenBuffers(1, &vertexbuffer);
+  glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+  GLuint colorbuffer;
+  glGenBuffers(1, &colorbuffer);
+  glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
+  glEnable(GL_BLEND);
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+  do{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glUseProgram(programID);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0,  (void*)0);
+    glEnableVertexAttribArray(1);
+    glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0,  (void*)0);
+    glDrawArrays(GL_TRIANGLES, 0, 2*3); // 12*3 indices starting at 0 -> 12 triangles
+    glDisableVertexAttribArray(0);
+    glDisableVertexAttribArray(1);
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+  while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
+      glfwWindowShouldClose(window) == 0 );
+  glDeleteBuffers(1, &vertexbuffer);
+  glDeleteBuffers(1, &colorbuffer);
+  glDeleteVertexArrays(1, &VertexArrayID);
+  glDeleteProgram(programID);
+  glfwTerminate();
+  return 0;
+}
