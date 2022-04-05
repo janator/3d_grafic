b0815968ba5bcc70e34ad4be138034f3c971219b
janator <48880517+janator@users.noreply.github.com>
2022-04-05 21:30:59 +0300

    Update two_triangles.cpp

--- hw_two_triangles/two_triangles.cpp
+++ hw_two_triangles/two_triangles.cpp
@@ -27,7 +27,7 @@ int main(void) {
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-  window = glfwCreateWindow( 1024, 768, "Tutorial 02 - Red triangle", NULL, NULL);
+  window = glfwCreateWindow( 1024, 768, "HW - Two triangles", NULL, NULL);
   if( window == NULL ){
     fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
     getchar();
