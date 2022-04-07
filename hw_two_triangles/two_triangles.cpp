be3ce244be5a77e0e7779fc0f34cc99f4e016ccc
janator <48880517+janator@users.noreply.github.com>
2022-04-07 15:46:34 +0300

    Add files via upload

--- hw_two_triangles/two_triangles.cpp
+++ hw_two_triangles/two_triangles.cpp
@@ -47,7 +47,9 @@ int main(void) {
   GLuint VertexArrayID;
   glGenVertexArrays(1, &VertexArrayID);
   glBindVertexArray(VertexArrayID);
-  GLuint programID = LoadShaders( "VertexShader.vertexshader", "ColorFragmentShader.fragmentshader" );
+  GLuint programID_1 = LoadShaders( "VertexShader.vertexshader", "ColorFragmentShader.fragmentshader" );
+  GLuint programID_2 = LoadShaders( "VertexShader.vertexshader", "ColorFragmentShader2.fragmentshader" );
+
   static const GLfloat g_vertex_buffer_data[] = {
       -0.4f, -0.8f, 0.0f,
       -0.4f, 0.8f, 0.0f,
@@ -56,45 +58,35 @@ int main(void) {
       0.0f, 0.8f, 0.0f,
       0.8f, 0.0f, 0.0f
   };
-  static const GLfloat g_color_buffer_data[] = {
-      0.8f, 0.0f, 0.8f, 1,
-      0.8f, 0.0f, 0.8f, 1,
-      1.0f, 0.0f, 0.0f, 1,
-      1.0f, 1.0f, 0.0f, 0.5,
-      1.0f, 1.0f, 0.0f, 0.5,
-      0.0f, 1.0f, 0.0f, 0.5
-  };
   GLuint vertexbuffer;
   glGenBuffers(1, &vertexbuffer);
   glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
   glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
-  GLuint colorbuffer;
-  glGenBuffers(1, &colorbuffer);
-  glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
+
   glEnable(GL_BLEND);
   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
   do{
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glUseProgram(programID);
+    glUseProgram(programID_1);
     glEnableVertexAttribArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0,  (void*)0);
-    glEnableVertexAttribArray(1);
-    glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
-    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0,  (void*)0);
-    glDrawArrays(GL_TRIANGLES, 0, 2*3); // 12*3 indices starting at 0 -> 12 triangles
+    glDrawArrays(GL_TRIANGLES, 0, 3); // 12*3 indices starting at 0 -> 12 triangles
+    glUseProgram(programID_2);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0,  (void*)0);
+    glDrawArrays(GL_TRIANGLES, 3, 3);
     glDisableVertexAttribArray(0);
-    glDisableVertexAttribArray(1);
+
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
   while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
       glfwWindowShouldClose(window) == 0 );
   glDeleteBuffers(1, &vertexbuffer);
-  glDeleteBuffers(1, &colorbuffer);
   glDeleteVertexArrays(1, &VertexArrayID);
-  glDeleteProgram(programID);
+  glDeleteProgram(programID_2);
+  glDeleteProgram(programID_1);
   glfwTerminate();
   return 0;
 }
